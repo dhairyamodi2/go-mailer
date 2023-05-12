3a17e66938ac856ddb6a667f7c0bf6ebe67f0ff0
dhairyam2002 <dhairyam2002@gmail.com>
2023-05-12 17:19:14 +0530

    making it an importable package

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/smtp"
 )
 
@@ -11,13 +10,4 @@ func Mailer(from string, to string, subject string, body string, password string
 	var err = smtp.SendMail(host + ":" + port, auth, from, []string{to}, []byte(body))
 
 	return err
-}
-func main(){
-	log.Print("Sending Mail...")
-	var err = Mailer("ui20cs40@iiitsurat.ac.in", "dhairyam2002@gmail.com", "My subject", "Body", "ileoezejhktcjvoq", "smtp.gmail.com", "587")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Print("Sent successfully!")
-	}
 }
