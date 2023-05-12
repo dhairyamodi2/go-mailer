ef38b2a7264ca05e6f495e83fd3fcd1d5d159cc5
dhairyam2002 <dhairyam2002@gmail.com>
2023-05-13 00:34:37 +0530

    go routines and concurrency, evaluating performance

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
-package mailer
+package main
 
 import (
+	"fmt"
+	"log"
 	"net/smtp"
+	"sync"
+	"time"
 )
 
-func Mailer(from string, to string, subject string, body string, password string, host string, port string) error{
-	var auth = smtp.PlainAuth("", from, password, host)
+func Mailer(from string, to string, subject string, body string, password string, host string, port string, wg *sync.WaitGroup) {
 
-	var err = smtp.SendMail(host + ":" + port, auth, from, []string{to}, []byte(body))
+	// go func (){
+		var auth = smtp.PlainAuth("", from, password, host)
+		defer wg.Done()
+		var err = smtp.SendMail(host + ":" + port, auth, from, []string{to}, []byte(body))
+		log.Print(err)
+	// }()
+}
 
-	return err
+
+func main(){
+	fmt.Print(time.Now())
+	var wg sync.WaitGroup
+	for i := 1; i < 20; i++ {
+		wg.Add(1)
+		Mailer("ui20cs40@iiitsurat.ac.in", "dhairyam2811@gmail.com", "My subject", "Message", "ileoezejhktcjvoq", "smtp.gmail.com", "587", &wg)	
+	}
+	wg.Wait()
+	fmt.Print(time.Now())
+	
 }
