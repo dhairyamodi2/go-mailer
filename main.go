bcd4079eb7bee0063056cd2239e53e05106bc24c
dhairyam2002 <dhairyam2002@gmail.com>
2023-05-13 00:36:19 +0530

    adding go-routines for concurrent execution

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"net/smtp"
 )
 
-func Mailer(from string, to string, subject string, body string, password string, host string, port string) error{
-	var auth = smtp.PlainAuth("", from, password, host)
+func Mailer(from string, to string, subject string, body string, password string, host string, port string) {
 
-	var err = smtp.SendMail(host + ":" + port, auth, from, []string{to}, []byte(body))
-
-	return err
+	go func (){
+		var auth = smtp.PlainAuth("", from, password, host)
+	    smtp.SendMail(host + ":" + port, auth, from, []string{to}, []byte(body))
+	}()
+	
 }
